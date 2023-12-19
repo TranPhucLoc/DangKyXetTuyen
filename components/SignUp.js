11310d1361d6cfda4c04ff2df0919551ec464c85
TranPhucLoc <locb1910665@student.ctu.edu.vn>
2023-12-19 08:27:19 +0700

    Login UI

--- components/SignUp.js
+++ components/SignUp.js
@@ -6,8 +6,9 @@ import {
   View,
 } from 'react-native';
 import React, {useState} from 'react';
-import { Appbar } from 'react-native-paper';
 import Input from './InputText';
+import Header from './Header';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const App = () => {
   const initialState = {    
@@ -26,24 +27,23 @@ const App = () => {
   };
 
   return (
-    <Appbar style={styles.bottom}>
-      <View style={styles.main}>
-        <Input
-          onChangeText={text => handleOnchange(text, 'email')}
-          placeholder="Enter your email address"
-        />
-        <Input
-          onChangeText={text => handleOnchange(text, 'password')}
-          placeholder="Enter your password"
-          secureTextEntry={true}
-        />
-        <TouchableOpacity
-          style={styles.buttonContainer}
-          onPress={() => submitHandler()}>
-          <Text style={{}}>Submit</Text>
-        </TouchableOpacity>
-      </View>
-    </Appbar>
+    <SafeAreaProvider style={styles.main}> 
+      <Header/>
+      <Input
+        onChangeText={text => handleOnchange(text, 'email')}
+        placeholder="Enter your email address"
+      />
+      <Input
+        onChangeText={text => handleOnchange(text, 'password')}
+        placeholder="Enter your password"
+        secureTextEntry={true}
+      />
+      <TouchableOpacity
+        style={styles.buttonContainer}
+        onPress={() => submitHandler()}>
+        <Text style={{}}>Submit</Text>
+      </TouchableOpacity>
+    </SafeAreaProvider>
   );
 };
 
@@ -54,32 +54,29 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#ffd082',
   },
-  bottom: {
-    position: 'absolute',
-    left: 0,
-    right: 0,
-    bottom: 0,
-  },
-  textContainer: {
-    marginVertical: 5,
-    backgroundColor: 'white',
-    width: '80%',
-    borderRadius: 10,
-    paddingLeft: 10,
-    shadowColor: '#000',
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-    borderWidth: 2,
-    borderColor: '#f5a623',
-  },
-  text: {
-    color: 'black',
-  },
+  // header: {
+  //   position: 'absolute',
+  // },
+  // textContainer: {
+  //   marginVertical: 5,
+  //   backgroundColor: 'white',
+  //   width: '80%',
+  //   borderRadius: 10,
+  //   paddingLeft: 10,
+  //   shadowColor: '#000',
+  //   shadowOffset: {
+  //     width: 0,
+  //     height: 2,
+  //   },
+  //   shadowOpacity: 0.25,
+  //   shadowRadius: 3.84,
+  //   elevation: 5,
+  //   borderWidth: 2,
+  //   borderColor: '#f5a623',
+  // },
+  // text: {
+  //   color: 'black',
+  // },
   buttonContainer: {
     display: 'flex',
     justifyContent: 'center',
